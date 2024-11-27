83b12023c81b19e3ae2d8fbacf44b795f2e265a2
Nuno Maduro <enunomaduro@gmail.com>
2024-11-27 13:36:57 +0000

    wip

--- config/database.php
+++ config/database.php
@@ -166,7 +166,6 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
-            'scheme' => env('REDIS_SCHEME'),
         ],
 
     ],
