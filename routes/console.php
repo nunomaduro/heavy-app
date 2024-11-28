1d5af946df3397b8feca8624a5a7388620533b50
Nuno Maduro <enunomaduro@gmail.com>
2024-11-28 13:56:08 +0000

    wip

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,18 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Artisan::command('every-minute', function () {
+    $this->comment('Running "every-minute" schedule task. Current time: ' . now());
+})->purpose('Display an inspiring quote')->everyMinute();
+
+Artisan::command('every-minute-in-background', function () {
+    $this->comment('Running "every-minute-in-background" scheduled task. Current time: ' . now());
+})->purpose('Display an inspiring quote')->everyMinute()->runInBackground();
+
+Artisan::command('every-minute-in-one-server', function () {
+    $this->comment('[onOneServer] Running "every-minute" schedule task. Current time: ' . now());
+})->purpose('Display an inspiring quote')->everyMinute()->onOneServer();
+
+Artisan::command('every-minute-in-background-in-one-server', function () {
+    $this->comment('[onOneServer] Running "every-minute-in-background" scheduled task. Current time: ' . now());
+})->purpose('Display an inspiring quote')->everyMinute()->runInBackground()->onOneServer();
